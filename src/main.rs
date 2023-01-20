ab72236ea206cd6c038529bce62d9e6c51342be5
Gowtham2543 <gowtham.manoharan@outlook.com>
2023-01-20 20:11:59 +0530

    Errors redirected to stderror

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ fn main() {
     
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing argument: {err}");
+        eprintln!("Problem parsing argument: {err}");
         process::exit(1);
     });
     println!("Searching for {} in {}", config.query, config.path);
 
     if let Err(e) = run(config) {
-        println!("Application Error : {e}");
+        eprintln!("Application Error : {e}");
         process::exit(1);
     }
 }
